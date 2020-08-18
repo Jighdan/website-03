4411d4e22ed1ceab6fe331015bf972d95851a8cb
Reinny <reynsth@gmail.com>
2020-08-18 14:08:35 -0400

    Base

---
+++ assets/scripts/index.js
@@ -0,0 +1,26 @@
+// const axios = require("axios");
+// const apiUrl = "https://api.github.com/users/jighdan";
+
+
+// const getProfile = async () => {
+// 	const response = await axios.get(apiUrl);
+// 	const profile = {
+// 		// Data unpacking
+// 		"name": response.data.name,
+// 		"username": response.data.login,
+// 		"image": response.data.avatar_url,
+// 		"bio": response.data.bio,
+// 		"location": response.data.location,
+// 	};
+
+// 	return profile;
+// };
+
+// const content = {
+// 	// DOM elements selector
+// 	name: document.getElementById("profileName"),
+// 	username: document.getElementById("profileUsername"),
+// 	image: document.getElementById("profileImage"),
+// 	location: document.getElementById("profileLocation"),
+// 	bio: document.getElementById("profileBio")
+// };
