09fe0be4351a6699f7528e8f26cb1605062bd0cf
Reinny <reynsth@gmail.com>
2020-08-19 16:18:59 -0400

    0.1

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,33 @@
 <template>
-	<main>
+	<main class="main">
+		<BaseHeader />
+
 		<Nuxt />
+
+		<footer class="footer">
+			<p class="footer__text">
+				Made by
+				<a href="https://github.com/jighdan" class="text__link">Jighdan</a>
+			</p>
+		</footer>
 	</main>
 </template>
+
+<style lang="scss">
+.main {
+	color: #282828;
+	margin: 0;
+	padding: 0;
+	scroll-behavior: smooth;
+	user-select: none;
+}
+
+.footer {
+	padding: 0.25rem 0.5rem;
+	width: 100%;
+
+	.footer__text {
+		text-align: right;
+	}
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,10 @@
 <template>
-	<main class="main">
-		<SectionHeader />
-		<article class="main__content">
-			<SectionIntro />
-			<SectionSkills />
-			<SectionProjects />
-			<SectionContact />
-		</article>
-	</main>
+	<article class="main__content">
+		<SectionIntro />
+		<SectionSkills />
+		<SectionProjects />
+		<SectionContact />
+	</article>
 </template>
 
 <script>
@@ -15,14 +12,6 @@ export default {};
 </script>
 
 <style lang="scss">
-.main {
-	color: #282828;
-	margin: 0;
-	padding: 0;
-	scroll-behavior: smooth;
-	user-select: none;
-}
-
 .main__content {
 	margin: 0 calc(10vw + 2.5%);
 	min-height: 100vh;
