c8164bc80271b3f3b6787d08d7a08152510d11b3
Reinny <reynsth@gmail.com>
2020-08-27 15:02:23 -0400

    Added Meta tags (Closes #3)

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
 			{
 				hid: "description",
 				name: "description",
-				content: process.env.npm_package_description || "",
+				content: "I am Jighdan, an always-learning web developer.",
 			},
 			{
 				name: "title",
