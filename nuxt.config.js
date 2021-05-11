829a31c85116d2935c814fa73a7ce62117380dd8
Reinny <reynsth@gmail.com>
2021-05-11 14:55:44 -0400

    Wraping configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,53 +1,53 @@
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
-  target: "static",
+	// Target: https://go.nuxtjs.dev/config-target
+	target: "static",
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: "jighdan-portfolio",
-    htmlAttrs: {
-      lang: "en",
-    },
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
-    ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-  },
+	// Global page headers: https://go.nuxtjs.dev/config-head
+	head: {
+		title: "jighdan-portfolio",
+		htmlAttrs: {
+			lang: "en",
+		},
+		meta: [
+			{ charset: "utf-8" },
+			{ name: "viewport", content: "width=device-width, initial-scale=1" },
+			{ hid: "description", name: "description", content: "" },
+		],
+		link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+	},
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+	// Global CSS: https://go.nuxtjs.dev/config-css
+	css: [],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+	plugins: [],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+	// Auto import components: https://go.nuxtjs.dev/config-components
+	components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    "@nuxt/typescript-build",
-    // https://go.nuxtjs.dev/stylelint
-    "@nuxtjs/stylelint-module",
-    // https://go.nuxtjs.dev/tailwindcss
-    "@nuxtjs/tailwindcss",
-    // https://google-fonts.nuxtjs.org/
-    "@nuxtjs/google-fonts",
-  ],
+	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+	buildModules: [
+		// https://go.nuxtjs.dev/typescript
+		"@nuxt/typescript-build",
+		// https://go.nuxtjs.dev/stylelint
+		"@nuxtjs/stylelint-module",
+		// https://go.nuxtjs.dev/tailwindcss
+		"@nuxtjs/tailwindcss",
+		// https://google-fonts.nuxtjs.org/
+		"@nuxtjs/google-fonts",
+	],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+	// Modules: https://go.nuxtjs.dev/config-modules
+	modules: [],
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+	// Build Configuration: https://go.nuxtjs.dev/config-build
+	build: {},
 
-  // Google Fonts Configuration
-  googleFonts: {
-    prefetch: true,
-    families: {
-      "Fira+Sans+Condensed": [300, 400, 500],
-    },
-  },
+	// Google Fonts Configuration
+	googleFonts: {
+		prefetch: true,
+		families: {
+			"Fira+Sans+Condensed": [300, 400, 500],
+		},
+	},
 };
