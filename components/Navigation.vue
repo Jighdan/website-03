78a655adc7eb6fc0079ac4ce15c18929351e1d26
Reinny <reynsth@gmail.com>
2021-05-16 16:36:36 -0400

    Adding new routes to the navigation

---
+++ components/Navigation.vue
@@ -0,0 +1,12 @@
+<template>
+	<nav>
+		<NuxtLink to="/projects">Projects</NuxtLink>
+		<NuxtLink to="/contact">Contact</NuxtLink>
+	</nav>
+</template>
+
+<style lang="postcss" scoped>
+nav {
+	@apply flex justify-between items-center gap-x-3;
+}
+</style>
