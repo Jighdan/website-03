06fc93fb590ece6909ee92911661b602b8a05f0a
Reinny <reynsth@gmail.com>
2021-05-16 16:36:08 -0400

    Styling default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,14 @@
 <template>
-	<div class="layout-container">
-		<Nuxt />
+	<div class="layout-default">
+		<header v-if="$route.name !== 'index'" class="home-link">
+			<nav>
+				<NuxtLink to="/">Back</NuxtLink>
+			</nav>
+		</header>
+
+		<div class="layout-page">
+			<Nuxt />
+		</div>
 	</div>
 </template>
 
@@ -33,7 +41,15 @@ body {
 	margin: 0;
 }
 
-.layout-container {
+.layout-default {
+	@apply py-14;
+}
+
+.layout-page {
 	@apply min-h-full min-w-full;
+}
+
+.home-link {
+	@apply mb-5;
 }
 </style>
