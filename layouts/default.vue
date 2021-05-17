43587760b2205cbb15577df84f40c3906f8df967
Reinny <reynsth@gmail.com>
2021-05-16 20:04:18 -0400

    Seperating global styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,34 +13,6 @@
 </template>
 
 <style lang="postcss">
-html {
-	font-family: "Fira Sans Condensed", sans-serif;
-	font-size: 18px;
-	word-spacing: 1px;
-	-ms-text-size-adjust: 100%;
-	-webkit-text-size-adjust: 100%;
-	-moz-osx-font-smoothing: grayscale;
-	-webkit-font-smoothing: antialiased;
-	box-sizing: border-box;
-	@apply text-gray-900 bg-gray-50;
-}
-
-html,
-body {
-	@apply h-full w-full;
-}
-
-body {
-	@apply grid place-items-center;
-}
-
-*,
-*::before,
-*::after {
-	box-sizing: border-box;
-	margin: 0;
-}
-
 .layout-default {
 	@apply py-14;
 }
@@ -51,14 +23,5 @@ body {
 
 .home-link {
 	@apply mb-5;
-}
-
-.page-enter-active,
-.page-leave-active {
-	transition: opacity 0.5s;
-}
-.page-enter,
-.page-leave-to {
-	opacity: 0;
 }
 </style>
