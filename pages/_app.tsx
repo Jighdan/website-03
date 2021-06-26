8585eb79495b66fa47f22e7ee60c46cf7c2b2f67
Reinny <reynsth@gmail.com>
2021-06-25 22:20:48 -0400

    Website title & Favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
 import type { AppProps } from 'next/app';
+import Head from "next/head";
 import "@/styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+		<>
+			<Head>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<title>Reinny Almonte | Portfolio</title>
+				<meta
+					name="description"
+					content="Reinny Almonte is a dedicated software apprenticeship that appreciates learning and improving myself in order to tackle challenging and complicated problems, in order to shape, design and develop performant and human solutions."
+				/>
+			</Head>
+
+			<Component { ...pageProps } />
+		</>
+	)
 }
 export default MyApp
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,10 @@ class AppDocument extends Document {
 		return (
 			<Html>
 				<Head>
+					{ /* Favicon */ }
+					<link rel="shortcut icon" href="favicon.ico" />
+
+					{ /* Google Fonts */ }
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
 					<link href="https://fonts.googleapis.com/css2?family=Fira+Sans+Condensed:ital,wght@0,400;0,500;1,400&display=swap" rel="stylesheet" />
