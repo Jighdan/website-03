bded79ba994d8427e876cee8914e0d40221d7ac0
Reinny <reynsth@gmail.com>
2021-06-25 20:25:12 -0400

    Using Fira Sans Condensed as Font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,7 +13,7 @@ class AppDocument extends Document {
 				<Head>
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-					<link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@300;400;500&display=swap" rel="stylesheet" />
+					<link href="https://fonts.googleapis.com/css2?family=Fira+Sans+Condensed:ital,wght@0,400;0,500;1,400&display=swap" rel="stylesheet" />
 				</Head>
 				<body>
 					<Main />
