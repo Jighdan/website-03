d751375d6061f1d2c5799689e3c8fbcd3af2635d
Reinny <reynsth@gmail.com>
2021-06-25 20:06:20 -0400

    Defining Contact Page Styles

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,17 +1,20 @@
 import LayoutSecondary from "@/layouts/secondary";
 import * as Section from "./sections";
+import styles from "./styles.module.scss";
 
 const ContactPage = () => {
 	return (
 		<LayoutSecondary>
-			<header>
-				<h2>Contact</h2>
-			</header>
+			<div className={ styles["contact-page"] }>
+				<header>
+					<h2>Contact</h2>
+				</header>
 
-			<div>
-				<Section.EmailSection />
-				<Section.SocialMediaSection />
-				<Section.AlgorithmsPlatformsSection />
+				<div className={ styles["contact-page-body"]}>
+					<Section.EmailSection />
+					<Section.SocialMediaSection />
+					<Section.AlgorithmsPlatformsSection />
+				</div>
 			</div>
 		</LayoutSecondary>
 	);
