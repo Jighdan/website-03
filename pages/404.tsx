d851706f96d1a0bf903e77869eff3db56c441369
Reinny <reynsth@gmail.com>
2021-06-25 22:28:54 -0400

    Handling invalid Routes

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import LayoutSecondary from "@/layouts/secondary";
+
+const ErrorPage = () => {
+	return (
+		<LayoutSecondary>
+			<h1>Page not Found.</h1>
+		</LayoutSecondary>
+	)
+};
+
+export default ErrorPage;
