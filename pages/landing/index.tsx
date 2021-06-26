2fdbc3e7b97d693a91d1f84840701445f42e9dbc
Reinny <reynsth@gmail.com>
2021-06-25 20:06:46 -0400

    Defining Landing Page Styles

--- pages/landing/index.tsx
+++ pages/landing/index.tsx
@@ -1,11 +1,12 @@
 import LayoutDefault from "@/layouts/default";
+import styles from "./styles.module.scss";
 
 const LandingPage = () => {
 	return (
 		<LayoutDefault>
-			<div>
+			<div className={ styles["landing-page"] }>
 				<h1>Reinny Almonte</h1>
-				<p>Hey ~ I<span>&#39;</span>m a Software Engineer from the Dominican Republic 🇩🇴</p>
+				<p>Hey ~ I<span>&#39;</span>m a Software Apprenticeship from the Dominican Republic 🇩🇴</p>
 			</div>
 		</LayoutDefault>
 	)
